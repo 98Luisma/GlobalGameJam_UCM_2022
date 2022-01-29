083f7459cd60369ea827eb04bb71d22a65124390
Luis S <54810123+98Luisma@users.noreply.github.com>
2022-01-29 19:28:28 +0100

    Fixed menus

--- Assets/OurAssets/Scripts/MainMenu.cs
+++ Assets/OurAssets/Scripts/MainMenu.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 1;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("MainScene");
