74154088831dc929343e0e89f7858230ecd04a57
Mumo <56559705+Miguemumo@users.noreply.github.com>
2022-01-29 16:07:50 +0100

    Added life, money and score

--- Assets/OurAssets/Scripts/Manager/GameManager.cs
+++ Assets/OurAssets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,11 +14,15 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Camera _mainCamera = null;
     [SerializeField] private PlayerController _player = null;
 
+    [SerializeField] private Canvas _mainCanvas = null;
+    private Transform lives = null;
+
     private static GameManager _instance;
     public static GameManager Instance { get => _instance; }
 
     private int life;
     private int money;
+    private int score;
 
 #region Getters
     public Camera MainCamera { get => _mainCamera; }
@@ -41,8 +46,10 @@ public class GameManager : MonoBehaviour
     {
         // _popupSpawner.SetShouldSpawn(true);
         // _enemySpawner.SetShouldSpawn(true);
-        life = 10;
+        life = 5;
         money = 1000;
+        score = 0;
+        lives = _mainCanvas.transform.Find("Lives");
     }
 
     void addMoney(int amount)
@@ -53,5 +60,21 @@ public class GameManager : MonoBehaviour
     void addLife(int amount)
     {
         life += amount;
+        int children = lives.childCount;
+
+        for (int i = 0; i < children; ++i)
+        {
+            if (i < life)
+                lives.transform.GetChild(i).GetComponent<RawImage>().color = Color.white;
+            else
+                lives.transform.GetChild(i).GetComponent<RawImage>().color = Color.black;
+        }
+
+        //_mainCanvas.transform.Find("Lives").Find("Hearth" + totalLives).gameObject.GetComponent<RawImage>().color = Color.black;
+    }
+
+    void addScore(int amount)
+    {
+        score += amount;
     }
 }
